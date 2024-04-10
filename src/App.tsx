4b0e562e4c3fb97021639fc8b5bb7da8a119e128
Kayky Dion <kayky.dion@gmail.com>
2024-04-09 21:52:56 -0300

    Convertendo estilização do projeto para o styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import { Container, GlobalStyle } from './styles'
+import lightTheme from './themes/light'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/Vaga/styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Card = styled.li`
+  border: 1px solid ${props => props.theme.primaryColor};
+  background-color: ${props => props.theme.secondaryColor};
+  color: ${props => props.theme.primaryColor};
+  padding: 16px;
+  transition: all ease .3s;
+  border-radius: 8px;
+
+  :hover {
+    background-color: ${props => props.theme.primaryColor};
+    color: ${props => props.theme.secondaryColor};
+
+    a {
+      border-color: ${props => props.theme.primaryColor};
+      background-color: ${props => props.theme.secondaryColor};
+      color: ${props => props.theme.primaryColor};
+    }
+  }
+
+  h3 {
+    font-weight: bold;
+    margin-bottom: 16px;
+  }
+
+  a {
+    border-color:  ${props => props.theme.secondaryColor};
+    background-color: ${props => props.theme.primaryColor};
+    color:  ${props => props.theme.secondaryColor};
+    display: inline-block;
+    padding: 8px 16px;
+    text-decoration: none;
+    margin-top: 16px;
+    font-weight: bold;
+    font-size: 14px;
+    border-radius: 8px;
+    text-align: center;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+
+  }
+`
